1f0f0ec6fd353c70b8620324cd3d0e4a1bd07278
Thomas Saied <tomas.temo77@gmail.com>
2022-02-14 12:08:59 +0200

    add update and delete objects

--- database/products_crud.py
+++ database/products_crud.py
@@ -1,3 +1,4 @@
+from bson.objectid import ObjectId
 from .connection import products_collection
 
 
@@ -10,7 +11,7 @@ class ProductsReposatory:
     #     return object_mapped
 
     async def all_products(self, page_size=10, page_num=1):
-        skips = page_size * (page_num -1)
+        skips = page_size * (page_num - 1)
         return products_collection.find().skip(skips).limit(page_size)
 
     async def add_product(self, product: dict):
@@ -18,17 +19,27 @@ class ProductsReposatory:
         return product_obj
 
     async def get_product(self, fields: dict):
-        fields = await self.validate_get_fields(fields=fields)
-        student = await products_collection.find_one(**fields)
-        return student
+        fields = self.validate_get_fields(fields=fields)
+        product = await products_collection.find_one(fields)
+        return product
 
     def validate_get_fields(self, fields: dict):
         keys = list(fields.keys())
         if "id" in keys and "_id" not in keys:
             id_value = fields.get("id", "")
-            fields["_id"] = id_value
+            fields["_id"] = ObjectId(id_value)
             fields.pop("id")
         return fields
+
+    async def update_product(self, pk: str, data: dict):
+        updated_obj = await products_collection.update_one(
+            {"_id": ObjectId(pk)}, {"$set": data}
+        )
+        return updated_obj
+
+    async def delete_product(self, pk: str):
+        delete = await products_collection.delete_one({"_id": ObjectId(pk)})
+        return delete
 
 
 ProductCRUD = ProductsReposatory()
