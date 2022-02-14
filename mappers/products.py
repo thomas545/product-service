f10cc90f6993a14b969ef7a9ada75356239c4d55
Thomas Saied <tomas.temo77@gmail.com>
2022-02-14 12:09:48 +0200

    fixs: - fix mapping function - add reterive single product - add update product - add delete product

--- mappers/products.py
+++ mappers/products.py
@@ -1,16 +1,19 @@
+from core.response import error_response
 from database.products_crud import ProductCRUD
 from models.products import RetrieveProduct
 
 
 def product_mapper(product: dict) -> dict:
-    return {
-        "id": "%s"% product.get("_id", ""),
-        "title": product.get("title", ""),
-        "description": product.get("description", ""),
-        "category_name": product.get("category_name", ""),
-        "price": product.get("price", 0),
-        "quantity": product.get("quantity", 0),
-    }
+    if product:
+        return {
+            "id": "%s" % product.get("_id", ""),
+            "title": product.get("title", ""),
+            "description": product.get("description", ""),
+            "category_name": product.get("category_name", ""),
+            "price": product.get("price", 0),
+            "quantity": product.get("quantity", 0),
+        }
+    return {}
 
 
 async def all_products(page_size=10, page_num=1):
@@ -25,3 +28,28 @@ async def all_products(page_size=10, page_num=1):
 async def create_product(product: dict):
     product_obj = await ProductCRUD.add_product(product)
     return product_obj.inserted_id
+
+
+async def get_single_product(pk: str):
+    product = await reterive_product(pk)
+    return product_mapper(product)
+
+
+async def reterive_product(pk):
+    get_product = await ProductCRUD.get_product({"id": pk})
+    if not get_product:
+        error_response("Product doesn't found!", 404)
+    return get_product
+
+
+async def update_product(pk: str, data: dict):
+    await reterive_product(pk)
+    update_product = await ProductCRUD.update_product(pk, data)
+    if not update_product:
+        error_response("Product doesn't updated!")
+    return update_product
+
+
+async def delete_product_with_pk(pk: str):
+    await reterive_product(pk)
+    await ProductCRUD.delete_product(pk)
